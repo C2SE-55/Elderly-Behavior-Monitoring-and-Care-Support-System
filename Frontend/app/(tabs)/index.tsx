b774beb72675e45111403e1d9464c1c9f812246d
C1SE17 <votrongkhue.220904@gmail.com>
2026-03-05 19:04:08 +0700

    merge code fe

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
-import Welcome from '../welcome';
-import Login from '../login';
-import Signup from '../signup';
-import { Redirect } from 'expo-router';
+// import Welcome from '../welcome';
+// import Login from '../login';
+// import Signup from '../signup';
+// import { Redirect } from 'expo-router';
 
 // export default function Index() {
 //   return <Redirect href="/welcome" />;
@@ -15,3 +15,16 @@ import { Redirect } from 'expo-router';
 // export default function Index() {
 //   return <Redirect href="/(tabs)/profile" />;
 // };
+import Welcome from '../../components/auth/welcome';
+import Login from '../../components/auth/login';
+import Signup from '../../components/auth/signup';
+import { Redirect } from 'expo-router';
+
+export default function Index() {
+  return <Redirect href="/health" />;
+}
+export {
+  Welcome,
+  Login,
+  Signup,
+};
