f0ac8e7e407859ba865b94a29d0729510eeb3272
C1SE17 <votrongkhue.220904@gmail.com>
2026-03-05 18:53:11 +0700

    Refactor

--- Frontend/app/(tabs)/index.tsx
+++ Frontend/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
-import Welcome from '../../components/auth/welcome';
-import Login from '../../components/auth/login';
-import Signup from '../../components/auth/signup';
+import Welcome from '../welcome';
+import Login from '../login';
+import Signup from '../signup';
 import { Redirect } from 'expo-router';
 
 // export default function Index() {
@@ -12,15 +12,6 @@ import { Redirect } from 'expo-router';
 //   Login,
 //   Signup,
 // };
-export default function Index() {
-<<<<<<< HEAD
-  return <Redirect href="/(tabs)/profile" />;
-=======
-  return <Redirect href="/health" />;
-}
-export {
-  Welcome,
-  Login,
-  Signup,
->>>>>>> ec5d54600258ba22dab12e2b1f8ac294d48ab000
-};
+// export default function Index() {
+//   return <Redirect href="/(tabs)/profile" />;
+// };
--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,11 +1,8 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-<<<<<<< HEAD
+
   return <Redirect href="/(tabs)/profile" />;
 }
-=======
+
   // return <Redirect href="/welcome" />;
-  return <Redirect href="/health" />;
-}
->>>>>>> ec5d54600258ba22dab12e2b1f8ac294d48ab000
